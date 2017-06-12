09e52726b4385b0bb418df4280f5424debf7cea6
Ferran <ferrandinand@gmail.com>
2017-06-12 23:32:50 +0000

    Create full name method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   before do
     @user = User.create(email: "test@test.com", password: "eytodoe", password_confirmation: "eytodoe", first_name: "Jhon", last_name: "Snow")
   end
+
   describe "creation" do
       it "can be created" do
           expect(@user).to be_valid
@@ -13,6 +14,12 @@ RSpec.describe User, type: :model do
           @user.last_name = nil
           expect(@user).to_not be_valid
       end
+  end
+
+  describe "custom name methods" do
+    it "has a full name method that combine firts and last name" do
+      expect(@user.full_name).to eq("SNOW,JHON")
+    end
   end
 end
 
